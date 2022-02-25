4718a94eb4cd6b54da4814bc6d829f9c9a21990f
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-02-25 20:33:32 +0530

    [Guru] Added program to calculate fare for a journey -UC1

--- CabInvoiceGeneratorProgram/Program.cs
+++ CabInvoiceGeneratorProgram/Program.cs
@@ -11,6 +11,9 @@ namespace CabInvoiceGeneratorProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator Program");
+
+            CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator(RideType.NORMAL);
+            Console.WriteLine(cabInvoiceGenerator.CalculateFare(10, 15));
             Console.ReadLine();
         }
     }
---
+++ CabInvoiceGeneratorTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CabInvoiceGeneratorTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("CabInvoiceGeneratorTesting")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2d9dd447-7f6c-4dd9-8e69-efab7dbc356e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
